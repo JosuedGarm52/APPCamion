98fbb61d662076c22135cc0264fda07d68a208fb
JosuedGarm52 <jaccobjosue@gmail.com>
2024-05-27 20:01:37 -0500

    1.3.1 cambio gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.kotlinKapt)
+    id("com.android.application") version "8.3.2"
+    id("org.jetbrains.kotlin.android") version "1.6.21"
     id("androidx.navigation.safeargs.kotlin")
-    id("com.google.devtools.ksp")
+    //id("com.google.devtools.ksp") version "1.7.0"
 }
 
 android {
@@ -48,29 +47,29 @@ android {
 
 
 dependencies {
-    implementation(libs.androidxCoreKtx)
-    implementation(libs.androidxAppcompat)
-    implementation(libs.material)
-    implementation(libs.androidxActivity)
-    implementation(libs.androidxConstraintlayout)
-    implementation(libs.androidxNavigationFragmentKtx)
-    implementation(libs.androidxNavigationUiKtx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidxJunit)
-    androidTestImplementation(libs.androidxEspressoCore)
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
     // Room dependencies
-    implementation(libs.roomRuntime)
-    implementation(libs.roomKtx)
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     // LiveData
-    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation(libs.glide)
+    implementation("com.github.bumptech.glide:glide:4.14.2")
 
     val lifeCycle = "2.7.0"
 
@@ -78,9 +77,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifeCycle")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifeCycle")
     implementation("androidx.lifecycle:lifecycle-common-java8:$lifeCycle")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifeCycle")
 
     // To use Kotlin Symbol Processing (KSP)
-    //ksp("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
 
